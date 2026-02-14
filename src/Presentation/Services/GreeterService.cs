3cc35b97f924575a2d43c3ee6931f59c32abe9b9
Daniil <dnloeinik@gmail.com>
2026-02-14 15:00:50 +0300

    Initial commit

---
+++ src/Presentation/Services/GreeterService.cs
@@ -0,0 +1,18 @@
+using System.Text.Json.Nodes;
+using Grpc.Core;
+
+namespace Presentation.Services;
+
+public class GreeterService(ILogger<GreeterService> logger) : Greeter.GreeterBase
+{
+    public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+    {
+        logger.LogInformation("The message is received from {Name}", request.Name);
+
+        return Task.FromResult(
+            new HelloReply
+            {
+                Message = "Hello " + request.Name
+            });
+    }
+}
